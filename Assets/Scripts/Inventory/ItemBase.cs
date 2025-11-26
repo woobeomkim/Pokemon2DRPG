fde2f98ad8ea129c6bbfa495903c33c8b85188ce
woobum <woobum72@gmail.com>
2025-11-27 04:50:01 +0900

    InventoryUI 구현

---
+++ Assets/Scripts/Inventory/ItemBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+    [SerializeField] Sprite icon;
+
+    public string Name => name;
+    public string Description => description;
+    public Sprite Icon => icon;
+}
---
+++ Assets/Scripts/Inventory/RecoveryItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Create new recovery Item")]
+public class RecoveryItem : ItemBase
+{
+    [Header("HP")]
+    [SerializeField] int hpAmount;
+    [SerializeField] bool restoreMaxHP;
+
+    [Header("PP")]
+    [SerializeField] int ppAmount;
+    [SerializeField] bool restoreMaxPP;
+
+    [Header("Status Condition")]
+    [SerializeField] ConditionID status;
+    [SerializeField] bool recoverAllStatus;
+
+    [Header("Revive")]
+    [SerializeField] bool revive;
+    [SerializeField] bool maxRevive;
+
+}
