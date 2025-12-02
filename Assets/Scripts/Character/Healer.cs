88587f0904e84eea4672e537b396cece3237930c
woobum <woobum72@gmail.com>
2025-12-03 00:24:48 +0900

    동적 Choice Box UI , 스크립트 구현

--- Assets/Scripts/Character/Healer.cs
+++ Assets/Scripts/Character/Healer.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Healer : MonoBehaviour
 {
-    public IEnumerator Heal(Transform player,Dialog dialog)
+    public IEnumerator Heal(Transform player, Dialog dialog)
     {
-        yield return DialogManager.i.ShowDialog(dialog);
+        int selectedChoice = 0;
+        yield return DialogManager.i.ShowDialog(dialog, new List<string> { "Yes", "No" },
+          (choiceIndex) => { selectedChoice = choiceIndex; });
 
-        yield return Fader.i.FadeIn(0.5f);
-        var playerParty = player.GetComponent<PokemonParty>();
-        playerParty.Pokemons.ForEach(p => p.Heal());
-        playerParty.PartyUpdate();
-        yield return Fader.i.FadeOut(0.5f);
+        if (selectedChoice == 0)
+        {
+            yield return Fader.i.FadeIn(0.5f);
+            var playerParty = player.GetComponent<PokemonParty>();
+            playerParty.Pokemons.ForEach(p => p.Heal());
+            playerParty.PartyUpdate();
+            yield return Fader.i.FadeOut(0.5f);
+            yield return DialogManager.i.ShowDialogText($"잘쉬어서 건강해보이네~ ");
+        }
+        else if (selectedChoice == 1)
+        {
+            yield return DialogManager.i.ShowDialogText($"언제든 쉬고싶으면 말걸어줘~");
+        }
     }
 }
